d5a350df0cb73e36229ba7136385b5ab69efa2cc
KaijuKoders <avishkar.yelave@nciportal.com>
2024-06-21 14:42:30 +0530

    sub commit

--- Calculator.java
+++ Calculator.java
@@ -10,11 +10,16 @@ public class Calculator
 	{
 		System.out.println((a+b+c));
 	}
+	void sub(int a, int b, int c)
+	{
+		System.out.println((a-b-c));
+	} 
 	
 	public static void main(String[] args) 
 	{
 		Calculator cal = new Calculator();
 		cal.average(5, 6, 7,3);
 		cal.add(8, 5, 2);
+		cal.sub(8, 5, 2);
 	}
 }
